d6d041a347a4c0e7d35a80fa239ea7e5bafb7739
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-05-15 10:55:12 +0530

    update16

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const cors = require("cors");
 const multer = require("multer");
-const path = require("path");
+// const path = require("path");
 const user = require("./routes/userRoutes");
 const post = require("../src/routes/postRoutes");
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 // app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(multer().any());
-app.use(express.static(path.join(__dirname, "../build")));
+// app.use(express.static(path.join(__dirname, "../build")));
 // app.use(express.static(path.join(__dirname, "../build")));
 app.use("/api/users", user);
 app.use("/api/post", post);
@@ -20,10 +20,10 @@ app.listen(port, () => {
   console.log("server start");
 });
 
-app.get("*", (req, res) => {
-  const file = path.join(__dirname, "../build/index.html");
-  res.sendFile(file);
-});
+// app.get("*", (req, res) => {
+//   const file = path.join(__dirname, "../build/index.html");
+//   res.sendFile(file);
+// });
 
 // app.get("*", (req, res) => {
 //   const file = path.join(__dirname, "../build/index.html");
